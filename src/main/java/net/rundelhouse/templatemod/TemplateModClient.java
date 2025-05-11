1124562f20919365c798ddc1fa94f158074e6d45
Javier Gómez <137917002+RundelCode@users.noreply.github.com>
2025-05-11 18:40:32 -0500

    Creatión of the folder

---
+++ src/main/java/net/rundelhouse/templatemod/TemplateModClient.java
@@ -0,0 +1,10 @@
+package net.rundelhouse.templatemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TemplateModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
